343a968fcb1dc00a0083a797f46d690caa8c7271
Pranshu1sati <95905172+Pranshu1sati@users.noreply.github.com>
2022-08-24 10:41:08 +0530

    Optimized Code also here

---
+++ LeetCode42TwoPointer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+       int i=0, j= height.length-1,maxLeft=0, maxRight=0, water =0;
+        while(i<j){
+            if(height[i]<=height[j]){//maxleft
+                maxLeft= Math.max(maxLeft,height[i]);
+                water+=maxLeft-height[i];
+                i++;
+            }
+            else{
+                //maxRight
+                maxRight=Math.max(maxRight,height[j]);
+                water+=maxRight-height[j];
+                j--;
+            }
+        }
+        return water;
+    }
+}
